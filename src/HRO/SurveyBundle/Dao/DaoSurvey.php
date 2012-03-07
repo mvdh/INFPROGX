0f51de5c9b11510dabbc65d3648eff59b7738fc7
Maarten van den Hoek <maartenvandenhoek@gmail.com>
2012-03-07 20:52:38 +0100

    Added/changed methods in DaoSurvey and DaoUser

--- src/HRO/SurveyBundle/Dao/DaoSurvey.php
+++ src/HRO/SurveyBundle/Dao/DaoSurvey.php
@@ -2,11 +2,31 @@
 
 namespace HRO\SurveyBundle\Dao;
 
-use Doctrine\ORM\EntityManager;
 use HRO\SurveyBundle\Entity\Survey;
 
 class DaoSurvey extends Dao {
 	
+	/**
+	 * Finds the surveys of an owner ((a user), based on the owners unique identifier.
+	 * @param Integer $id The unique identifier for an owner
+	 * @return An array with one or more survey if they exists. Otherwise it returns Null 
+	 */
+	public function findByOwner($id) {
+		return $this->repo->findByOwner($id);
+	}
+	
+	/**
+	 * Finds the survey with the given title. Titles are unique, so there will be only one.
+	 * @param String $title The given title
+	 * @return A Survey if a survey was found, otherwise Null
+	 */
+	public function findOneByTitle($title) {
+		return $this->repo->findOneByTitle($title);
+	}
+	
+	/**
+	 * @see Dao
+	 */
 	public function __construct($container) {
 		parent::__construct($container);
 		$this->repo = $this->em->getRepository('HROSurveyBundle:Survey');
--- src/HRO/SurveyBundle/Dao/DaoUser.php
+++ src/HRO/SurveyBundle/Dao/DaoUser.php
@@ -6,6 +6,27 @@ use HRO\SurveyBundle\Entity\User;
 
 class DaoUser extends Dao {
 	
+	/**
+	 * Finds a user by it's emailaddress.
+	 * @param String $emailAddress
+	 * @return A user if found, otherwise null
+	 */
+	public function findOneByEmailAddress($emailAddress) {
+		return $this->repo->findOneByEmailAddress($emailAddress);
+	}
+	
+	/**
+	 * Finds a user by it's username.
+	 * @param String $username
+	 * @return A user if founc, otherwise null
+	 */
+	public function findOneByUsername($username) {
+		return $this->repo->findOneByUsername($username);
+	}
+	
+	/**
+	 * @see Dao
+	 */
 	public function __construct($container) {
 		parent::__construct($container);
 		$this->repo = $this->em->getRepository('HROSurveyBundle:User');
